0dc8e682d56e888b658f872e28a4d9326aa52ab9
frobee <frobi.webdev@outlook.com>
2023-04-10 17:08:38 +0930

    create-constructor-function-for-itemising-books

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-function sayHi(name) {}
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  myLibrary.push(new Book(title, author, pages, read));
+}
+
+addBookToLibrary('Where The Wild Things Are', 'Mauriece Sendak', '40', 'Yes');
+
+addBookToLibrary('Think Like A Programmer', 'V. Anton Spraul', '256', 'Yes');
+
+console.log(myLibrary);
