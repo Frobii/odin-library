b6db40719e375f7c9382afc8287eeb9c5c34439e
frobee <frobi.webdev@outlook.com>
2023-04-11 17:20:31 +0930

    create-a-table-and-a-way-to-populate-it

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const myLibrary = [];
+const bookTable = document.getElementById('book-table');
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -11,8 +12,33 @@ function addBookToLibrary(title, author, pages, read) {
   myLibrary.push(new Book(title, author, pages, read));
 }
 
-addBookToLibrary('Where The Wild Things Are', 'Mauriece Sendak', '40', 'Yes');
+function populateTable(array, table) {
+  array.forEach((item) => {
+    const row = table.insertRow();
 
-addBookToLibrary('Think Like A Programmer', 'V. Anton Spraul', '256', 'Yes');
+    const titleCell = row.insertCell();
+    titleCell.textContent = item.title;
 
-console.log(myLibrary);
+    const authorCell = row.insertCell();
+    authorCell.textContent = item.author;
+
+    const statusCell = row.insertCell();
+    statusCell.textContent = item.read;
+  });
+}
+
+addBookToLibrary(
+  'Where The Wild Things Are',
+  'Mauriece Sendak',
+  '40',
+  'Complete'
+);
+
+addBookToLibrary(
+  'Think Like A Programmer',
+  'V. Anton Spraul',
+  '256',
+  'Ongoing'
+);
+
+populateTable(myLibrary, bookTable);
