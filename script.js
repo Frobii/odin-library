971dd44a9776eb59a2304c7bc242fb0e4eddc175
frobee <frobi.webdev@outlook.com>
2023-04-12 19:23:02 +0930

    link-the-form-submission-to-the-table-ignoring-duplicates

--- script.js
+++ script.js
@@ -1,51 +1,74 @@
 const myLibrary = [];
 const bookTable = document.getElementById('book-table');
+const form = document.getElementById('book-form');
 
-function Book(title, author, pages, read) {
+function Book(title, author, readingStatus) {
   this.title = title;
   this.author = author;
-  this.pages = pages;
-  this.read = read;
+  this.readingStatus = readingStatus;
 }
 
-function addBookToLibrary(title, author, pages, read) {
-  myLibrary.push(new Book(title, author, pages, read));
+function addBookToLibrary(title, author, read) {
+  myLibrary.push(new Book(title, author, read));
 }
 
-function populateTable(array, table) {
-  array.forEach((item) => {
-    const row = table.insertRow();
+function populateTable(title, author, readingStatus, table = bookTable) {
+  const row = table.insertRow();
 
-    const titleCell = row.insertCell();
-    titleCell.textContent = item.title;
+  const titleCell = row.insertCell();
+  titleCell.textContent = title;
 
-    const authorCell = row.insertCell();
-    authorCell.textContent = item.author;
+  const authorCell = row.insertCell();
+  authorCell.textContent = author;
 
-    const statusCell = row.insertCell();
-    statusCell.textContent = item.read;
+  const statusCell = row.insertCell();
+  statusCell.textContent = readingStatus;
+}
+
+function checkDuplicate(library, title, author) {
+  let duplicateFound = false;
+
+  library.forEach((item) => {
+    if (item.title === title && item.author === author) {
+      duplicateFound = true;
+    }
   });
+
+  return duplicateFound;
 }
 
-addBookToLibrary(
-  'Where The Wild Things Are',
-  'Mauriece Sendak',
-  '40',
-  'Complete'
-);
+function submitForm(event) {
+  event.preventDefault();
 
-addBookToLibrary(
-  'Thinking Fast and Slow',
-  'Daniel Kahneman',
-  '499',
-  'Not Started'
-);
+  const formData = new FormData(document.getElementById('book-form'));
 
-addBookToLibrary(
-  'Think Like A Programmer',
-  'V. Anton Spraul',
-  '256',
-  'In Progress'
-);
+  const title = formData.get('title');
+  const author = formData.get('author');
+  const readingStatus = formData.get('reading-status');
 
-populateTable(myLibrary, bookTable);
+  if (!checkDuplicate(myLibrary, title, author)) {
+    addBookToLibrary(title, author, readingStatus);
+    populateTable(title, author, readingStatus);
+  }
+}
+
+form.addEventListener('submit', submitForm);
+
+addBookToLibrary('Where The Wild Things Are', 'Mauriece Sendak', 'Complete');
+populateTable(
+  myLibrary[0].title,
+  myLibrary[0].author,
+  myLibrary[0].readingStatus
+);
+addBookToLibrary('Thinking Fast and Slow', 'Daniel Kahneman', 'Not Started');
+populateTable(
+  myLibrary[1].title,
+  myLibrary[1].author,
+  myLibrary[1].readingStatus
+);
+addBookToLibrary('Think Like A Programmer', 'V. Anton Spraul', 'In Progress');
+populateTable(
+  myLibrary[2].title,
+  myLibrary[2].author,
+  myLibrary[2].readingStatus
+);
